fba35c16068c1f682b76e3d4ad546b62db023c22
bugsbrog <bugsbrog@gmail.com>
2021-12-17 12:46:09 -0700

    First test done in Article.test.js

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import '@testing-library/jest-dom';
+import { screen, render } from '@testing-library/react';
 
 import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
 
-test('renders component without errors', ()=> {
+const articleTest = {
+    headline: 'headline test',
+    author: 'author test',
+    summary: 'summary test',
+    body: 'body test'
+}
+
+test('renders component without errors', () => {
+    render(<Article article={articleTest}/>)
 });
 
-// test('renders headline, author from the article when passed in through props', ()=> {
-// });
+test('renders headline, author from the article when passed in through props', () => {
+});
 
 // test('renders "Associated Press" when no author is given', ()=> {
 // });
