3817bbe9a1eb7648e6a379a91401157f09787763
bugsbrog <bugsbrog@gmail.com>
2021-12-17 11:47:14 -0700

    Got articles to show up and can now delete them

--- src/components/View.js
+++ src/components/View.js
@@ -1,26 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import Article from './Article';
 import EditForm from './EditForm';
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const View = (props) => {
     const [articles, setArticles] = useState([]);
     const [editing, setEditing] = useState(false);
     const [editId, setEditId] = useState();
 
+    useEffect(() => {
+        axiosWithAuth().get('/articles')
+            .then(res => {
+                setArticles(res.data);
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }, []);
+
     const handleDelete = (id) => {
-    }
+        axiosWithAuth().delete(`/articles/${id}`)
+            .then(res => {
+                setArticles(res.data);
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    };
 
     const handleEdit = (article) => {
+
     }
 
-    const handleEditSelect = (id)=> {
+    const handleEditSelect = (id) => {
         setEditing(true);
         setEditId(id);
     }
 
-    const handleEditCancel = ()=>{
+    const handleEditCancel = () =>{
         setEditing(false);
     }
 
@@ -47,9 +66,9 @@ const View = (props) => {
 export default View;
 
 //Task List:
-//1. Build and import axiosWithAuth module in the utils.
-//2. When the component mounts, make an http request that adds all articles to state.
-//3. Complete handleDelete method. It should make a request that delete the article with the included id.
+//1. Build and import axiosWithAuth module in the utils. X
+//2. When the component mounts, make an http request that adds all articles to state.X
+//3. Complete handleDelete method. It should make a request that delete the article with the included id.X
 //4. Complete handleEdit method. It should make a request that updates the article that matches the included article param.
 
 
